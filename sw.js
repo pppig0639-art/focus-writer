93f55c589777f4ba7ec9d2dbc16972d8b7ce4735
pppig0639-art <pppig0639@gmail.com>
2026-03-19 23:21:23 +0800

    Update service worker to version 2 and modify assets

--- sw.js
+++ sw.js
@@ -1,29 +1,29 @@
-const CACHE = 'focus-writer-v1';
+const CACHE = ‘focus-writer-v2’;
 const ASSETS = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  'https://fonts.googleapis.com/css2?family=Noto+Serif+TC:wght@300;400&family=IM+Fell+English:ital@0;1&display=swap'
+‘/focus-writer/’,
+‘/focus-writer/index.html’,
+‘/focus-writer/manifest.json’,
+‘/focus-writer/sw.js’
 ];
 
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE).then(cache => cache.addAll(ASSETS)).catch(() => {})
-  );
-  self.skipWaiting();
+self.addEventListener(‘install’, e => {
+e.waitUntil(
+caches.open(CACHE).then(cache => cache.addAll(ASSETS)).catch(() => {})
+);
+self.skipWaiting();
 });
 
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
-  );
-  self.clients.claim();
+self.addEventListener(‘activate’, e => {
+e.waitUntil(
+caches.keys().then(keys =>
+Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+)
+);
+self.clients.claim();
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => cached))
-  );
+self.addEventListener(‘fetch’, e => {
+e.respondWith(
+caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => cached))
+);
 });
